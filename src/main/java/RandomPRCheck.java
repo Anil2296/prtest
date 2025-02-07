85f755fdf76e5428f34368dbf1355bbad643691d
Anil2296 <anilkumarnagina007@gmail.com>
2025-02-07 11:53:38 +0530

    new

---
+++ src/main/java/RandomPRCheck.java
@@ -0,0 +1,24 @@
+package main.java;
+
+
+
+import java.util.Random;
+
+public class RandomPRCheck {
+    public static void main(String[] args) {
+        // Generate a random boolean value (true or false)
+        boolean randomResult = new Random().nextBoolean();
+
+        // Print the result
+        System.out.println("Random result: " + randomResult);
+
+        // Exit code 0 for success, 1 for failure
+        if (randomResult) {
+            System.out.println("OK, you can merge!");
+            System.exit(0);  // Success exit code (OK to merge)
+        } else {
+            System.out.println("Sorry, you cannot merge.");
+            System.exit(1);  // Failure exit code (Cannot merge)
+        }
+    }
+}
